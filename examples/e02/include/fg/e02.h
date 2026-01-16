c7313562a373bbb3c4519740b9aba62c87d7f69c
llba <a@b>
2026-01-16 16:57:31 +0800

    1

---
+++ examples/e02/include/fg/e02.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "fg/util.h"
+#include "fg/ogre.h"
+#include "fg/demo.h"
+namespace fog
+{
+    struct Example02
+    {
+
+        INJECT(Example02(CoreMod *core, Ogre::SceneNode *sceNode))
+        {
+            Ogre::ManualObject *obj = core->createManualObject();
+            sceNode->attachObject(obj);
+            obj->clear();
+            obj->begin("E00Material", Ogre::RenderOperation::OT_TRIANGLE_LIST);
+            int baseIdx = obj->getCurrentVertexCount();
+            Vector3 a{0, 0, 0};
+            Vector3 b{1, 0, 0};
+            Vector3 c{0, 0, -1};
+
+            obj->position(a);
+            obj->position(b);
+            obj->position(c);
+
+            obj->triangle(baseIdx + 0, baseIdx + 1, baseIdx + 2);
+
+            obj->end();
+            sceNode->setScale(30, 30, 30);
+        }
+
+        static int run(Options::Groups &ogs);
+    };
+};
