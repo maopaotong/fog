5b7787a78af843e5d53c42ff25f2d5eaa7502ac8
llba <a@b>
2026-01-24 15:05:33 +0800

    1

--- examples/e03/materials/e03_01_fragment.glsl
+++ examples/e03/materials/e03_01_fragment.glsl
@@ -3,20 +3,13 @@
 uniform sampler2D tex_elevation;
 uniform sampler2D tex_colormap;
 
-in vec2 fUv;
-in float elevation;
+in float fE;
+in vec2 fPos;
 out vec4 color;
 void main() {
-    vec2 pos = fUv; 
     float m = 0;
-    float z = texture2D(tex_elevation, pos).r;
-    vec4 c1 = texture2D(tex_colormap, vec2(z, m));
-    color = c1;
-    // if(e >= 0.5) {
-    //     color = vec4(0, e, 0, 1);
-    // } else {
-    //     color = vec4(0, e, 0, 1);
-    // }
-
-    
+    float z = texture2D(tex_elevation, fPos).r;
+    vec2 uvC = vec2(fE, m);
+    vec4 c1 = texture2D(tex_colormap, uvC);
+    color = c1;       
 }
--- examples/e03/materials/e03_01_vertex.glsl
+++ examples/e03/materials/e03_01_vertex.glsl
@@ -5,13 +5,14 @@ in vec3 vertex;
 in vec2 uv0;//
 in vec2 uv1;//
 
-out float elevation;
-out vec2 fUv;
+out vec2 fElm;
+out vec2 fPos;
 void main() {
-
-	vec4 gPos = projection * vec4(vertex.xy, vertex.z * 1, 1.0);
-	//gPos = vec4(vertex.x * 0.001 * 0.25, -vertex.y * 0.001 * 0.5, -0.1, 1);
+	
+	vec4 gPos = projection * vec4(vertex, 1.0);
 	gl_Position = gPos;
-	fUv = vertex.xy * 0.001;
-	elevation = vertex.z;
+	float e = (vertex.z + 1.0) * 0.5;
+	fElm = vec2(e, 0.5);
+	fPos = vertex.xy * 0.001 + 1.0 / 64.0;	
+	fPos.y = 1.0 - fPos.y;//flip y
 }
